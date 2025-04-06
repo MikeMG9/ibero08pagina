130c3ea9dfa1ced35e5528b9edf4e7e64e0fa3ec
MikeMG9 <aizha.mg@gmail.com>
2025-04-06 15:50:07 -0500

    se hace prueb hola mundo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var app = express()
 
-
+console.log("Hola soy una prueba de respostirio en aws")
 
 app.listen(3000, function(){
     console.log("app runnig in local port 3000")
